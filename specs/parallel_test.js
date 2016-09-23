cb831b8844e8abd81fdabf04ef7da86a47ae7579
Harish Ved <ved.harish3@gmail.com>
2016-09-23 13:52:42 +0530

    Parallel Tests

--- specs/parallel_test.js
+++ specs/parallel_test.js
@@ -1,53 +1,41 @@
 var assert = require('assert'),
   webdriver = require('selenium-webdriver'),
-  conf_file = process.argv[3] || 'conf/single.conf.js';
-  
+  conf_file = process.argv[3] || 'conf/single.conf.js',
+  parallel = require('mocha.parallel'),
+  Promise = require('bluebird');
+
 var capabilities = require('../' + conf_file).capabilities;
 
 var buildDriver = function(caps) {
-  return new webdriver.Builder().
-    usingServer('https://hub-cloud.browserstack.com/wd/hub').
-    withCapabilities(caps).
-    build();
+  return new Promise(function(resolve, reject) {
+    var driver = new webdriver.Builder().
+      usingServer('https://hub-cloud.browserstack.com/wd/hub').
+      withCapabilities(caps).
+      build();
+    resolve(driver);
+  });
 };
 
-console.log(JSON.stringify(capabilities));
-describe('Tests ', function() {
+parallel('Tests ', function() {
+  var driver, bsLocal;
+
   capabilities.forEach(function(caps) {
-    console.log(JSON.stringify(caps));
-    it('Google\'s Search Functionality for ' + caps.browserName, function(higherDone) {
-      this.timeout(0);
-      var driver, bsLocal;
-
-      beforeEach(function(done) {
-        driver = buildDriver(caps);
-        done();
-      });
-
-      it('can find search results', function (done) {
+    it('can find search results', function (done) {
+      buildDriver(caps).then(function(driver) {
         driver.get('http://www.google.com/ncr').then(function() {
           driver.findElement(webdriver.By.name('q')).sendKeys('BrowserStack').then(function() {
             driver.findElement(webdriver.By.name('btnG')).click().then(function() {
               driver.getTitle().then(function(title) {
-                assert(title.match(/BrowserStack - Google Search/i) != null);
-                done();
+                setTimeout(function() {
+                  assert(title.match(/BrowserStack - Google Search/i) != null);
+                  driver.quit().then(function() {
+                    done();
+                  });
+                }, 5000);
               });
             });
           });
         });
-      });
-
-      afterEach(function(done) {
-        driver.quit().then(function() {
-          done();
-        });
-      });
-      afterAll(function() {
-        console.log("HIGHER!!!");
-        console.log("HIGHER!!!");
-        console.log("HIGHER!!!");
-        console.log("HIGHER!!!");
-        higherDone();
       });
     });
   });
