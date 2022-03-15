6f2c677de80206250807210eb6ea6fc81449400e
Emma Malysz <emmamalysz@Emmas-MacBook-Pro.local>
2022-03-15 08:00:54 -0700

    Fix typo in parallel.conf.js

--- conf/parallel.conf.js
+++ conf/parallel.conf.js
@@ -1,5 +1,5 @@
 var config = {
-  'commanCapabilities': {
+  'commonCapabilities': {
     'browserstack.user': process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
     'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
     'build': 'mocha-browserstack',
@@ -42,7 +42,7 @@ var config = {
 exports.capabilities = [];
 // Code to support common capabilities
 config.multiCapabilities.forEach(function(caps) {
-  var temp_caps = JSON.parse(JSON.stringify(config.commanCapabilities));
+  var temp_caps = JSON.parse(JSON.stringify(config.commonCapabilities));
   for(var i in caps) temp_caps[i] = caps[i];
   exports.capabilities.push(temp_caps);
 });
