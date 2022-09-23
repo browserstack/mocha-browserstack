fa4b2d7a4534b2ea5dc75df3a14fbdfd1401beff
Neha Agarwal <neha@Nehas-MacBook-Pro.local>
2022-09-23 20:20:35 +0530

    Added source capability

--- conf/local.conf.js
+++ conf/local.conf.js
@@ -3,6 +3,7 @@ exports.capabilities = {
   'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
   'build': 'mocha-browserstack',
   'name': 'local_test',
+  'browserstack.source': 'mocha:sample-selenium-3-v1.0',
   'browserstack.debug': 'true',
 
   'browserName': 'chrome',
