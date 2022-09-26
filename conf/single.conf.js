be076c0e5291500c3330192714d98fcc88e7477b
Neha Agarwal <neha@Nehas-MacBook-Pro.local>
2022-09-26 13:14:26 +0530

    Corrections

--- conf/single.conf.js
+++ conf/single.conf.js
@@ -1,11 +1,11 @@
 exports.capabilities = {
   'browserName': 'chrome',
   'bstack:options': {
-  'userName': process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
-  'accessKey': process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
-  'buildName': 'browserstack-build-1',
-  'sessionName': 'Bstack single mocha',
-  'debug': 'true',
-  'source': 'mocha:sample-selenium-4-v1.0'
+    'userName': process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
+    'accessKey': process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
+    'buildName': 'browserstack-build-1',
+    'sessionName': 'Bstack single mocha',
+    'debug': 'true',
+    'source': 'mocha:sample-selenium-4-v1.0'
   }
 };
