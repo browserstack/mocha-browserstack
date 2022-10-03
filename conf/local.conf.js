dbee48996bc0ae4fbdcea91f4a8f4c918ab19565
Neha Agarwal <neha@ip-192-168-1-5.ec2.internal>
2022-10-03 23:07:16 +0530

    Hardcoded source

--- conf/local.conf.js
+++ conf/local.conf.js
@@ -3,7 +3,6 @@ exports.capabilities = {
   'browserstack.key': process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
   'build': 'browserstack-build-1',
   'name': 'BStack local mocha',
-  'browserstack.source': 'mocha:sample-selenium-3-v1.0',
   'browserstack.debug': 'true',
 
   'browserName': 'chrome',
